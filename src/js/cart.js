cd0154183ddc4f0591184cb7e91477751b5fd9bc
Kiktamo <mccrary@gmx.us>
2023-09-29 12:09:34 -0400

    Make the cart subtotal/total dynamic

--- src/js/cart.js
+++ src/js/cart.js
@@ -1,4 +1,7 @@
-import { setLocalStorage, getLocalStorage } from './utils.mjs';
+import {
+  setLocalStorage,
+  getLocalStorage
+} from './utils.mjs';
 import updateCartCount from './updateCartCount.mjs';
 
 function renderCartContents() {
@@ -7,26 +10,12 @@ function renderCartContents() {
   if (cartItems && cartItems.length > 0) {
     const htmlItems = cartItems.map((item) => cartItemTemplate(item));
     document.querySelector('.product-list').innerHTML = htmlItems.join('');
+    updateCartFooter(cartItems);
+    renderCartSubtotal(cartItems);
 
-    //Remove hidden status on footer
-    document
-      .querySelector('.cart-footer-hidden')
-      .setAttribute('class', 'cart-footer');
-
-    // Add subtotal
-    let subtotal = 0;
-    cartItems.forEach((element) => {
-      subtotal += element.FinalPrice;
-    });
-
-    document
-      .querySelector('.cart-footer')
-      .insertAdjacentHTML(
-        'afterBegin',
-        `<p class="cart-total">Total: ${subtotal}</p></span>`
-      );
   } else {
     // Handle the case when the cart is empty
+    updateCartFooter(cartItems);
     document.querySelector('.product-list').innerHTML = 'Your cart is empty.';
   }
 
@@ -38,6 +27,43 @@ function renderCartContents() {
       renderCartContents(); // Re-render the cart after removal
     });
   });
+}
+
+function renderCartSubtotal(cartItems) {
+
+  // Add subtotal
+  let subtotal = 0;
+
+  cartItems.forEach((element) => {
+    subtotal += element.FinalPrice;
+  });
+
+  const cartTotal = document.querySelector('.cart-total')
+
+  if (cartTotal) {
+    cartTotal.innerHTML = `Total: ${subtotal}`
+  } else {
+    document
+      .querySelector('.cart-footer')
+      .insertAdjacentHTML(
+        'afterBegin',
+        `<p class="cart-total">Total: ${subtotal}</p></span>`
+      );
+  }
+}
+
+function updateCartFooter(cartItems) {
+  const hidden = document.querySelector('.cart-footer-hidden')
+  const count = cartItems.length
+
+  if (count > 0 && hidden) {
+    hidden.setAttribute('class', 'cart-footer');
+  } if (count <= 0 && !hidden) {
+    document
+    .querySelector('.cart-footer')
+    .setAttribute('class', 'cart-footer-hidden');
+  }
+
 }
 
 function cartItemTemplate(item) {
