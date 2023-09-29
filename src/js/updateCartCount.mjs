1e55ce21f9b6e6ce6fc6af8c2eedc9d60682ee68
Joel Cannon <joelcannon@mac.com>
2023-09-29 10:08:17 -0500

    fixed updateCartCount() and missing } in .css

--- src/js/updateCartCount.mjs
+++ src/js/updateCartCount.mjs
@@ -1,24 +1,20 @@
 import { getLocalStorage } from './utils.mjs';
 
-// Function to update the cart count
 export default function updateCartCount() {
   const cartElement = document.querySelector('.cart');
-  const cartCountElement = cartElement.querySelector('.cart-count'); // Find the cart count element
-
   const cartItems = getLocalStorage('so-cart');
-  const count = cartItems ? cartItems.length : 0; // Check if cartItems is not null
+  const count = cartItems ? cartItems.length : 0;
 
+  // Find the existing cart count element or create a new one
+  let cartCountElement =
+    cartElement.querySelector('.cart-count') || document.createElement('span');
+  cartCountElement.classList.add('cart-count');
+  cartCountElement.textContent = count.toString();
+
+  // Add or remove the cart count element based on the count value
   if (count > 0) {
-    const cartCountHTML = `<span class="cart-count">${count}</span>`;
-    
-    // Check if cartCountElement exists, and if it doesn't, insert the HTML
-    if (!cartCountElement) {
-      cartElement.insertAdjacentHTML('beforeend', cartCountHTML);
-    }
+    cartElement.appendChild(cartCountElement);
   } else {
-    // If count is 0 and cartCountElement exists, remove it
-    if (cartCountElement) {
-      cartCountElement.remove();
-    }
+    cartCountElement.remove();
   }
 }
