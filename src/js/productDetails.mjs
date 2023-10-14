69e0a89ebbd825266a26f60f8a29982ea0a829cd
jayhklasiq <joshvolx@gmail.com>
2023-10-14 19:59:44 +0000

    completed the search bar function.

--- src/js/productDetails.mjs
+++ src/js/productDetails.mjs
@@ -25,7 +25,7 @@ export default async function productDetails(productId) {
     }
 }
 
-function renderProductDetails(product) {
+export function renderProductDetails(product) {
   // Get references to the HTML elements
   const productName = document.getElementById('productName');
   const productNameWithoutBrand = document.getElementById(
