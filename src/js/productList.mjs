2fb479bfa2ba76e35074bd633f7433f81f745ddb
Kiktamo <mccrary@gmx.us>
2023-10-20 15:36:08 -0400

    Basic Quicklook

--- src/js/productList.mjs
+++ src/js/productList.mjs
@@ -1,6 +1,6 @@
 import { qs } from './utils.mjs';
-import { getData } from './productData.mjs';
-import { renderListWithTemplate } from './utils.mjs';
+import { getData, findProductById } from './productData.mjs';
+import { renderListWithTemplate, renderWithTemplate } from './utils.mjs';
 
 export default async function productList(
   selector,
@@ -16,36 +16,39 @@ export default async function productList(
 
   // Render the sorted product list
   renderListWithTemplate(productCardTemplate, product_list, products);
+
+  const elQuicklookButtons = document.querySelectorAll('.quick-look');
+  elQuicklookButtons.forEach((button) => {
+    button.addEventListener('click', (event) => {
+      const productId = event.currentTarget.getAttribute('data-id');
+      toggleQuicklook(productId, true);
+    });
+  });
 }
 
 function productCardTemplate(product) {
+  const baseTemplate = `<li class="product-card">
+  <img data-id="${product.Id}" class="quick-look" src="/images/magnify.svg alt="magnifying glass">
+  <a href="../product_pages/index.html?product=${product.Id}">
+  <img
+    src="${product.Images.PrimaryMedium}"
+    alt="Image of ${product.Name}"
+  />
+  <h3 class="card__brand">${product.Brand.Name}</h3>
+  <h2 class="card__name">${product.NameWithoutBrand}</h2>
+  <p class="product-card__price">$${product.FinalPrice}`;
+
   if (product.FinalPrice < product.ListPrice) {
     const discount =
       ((product.ListPrice - product.FinalPrice) / product.ListPrice) * 100;
-    return `<li class="product-card">
-      <a href="../product_pages/index.html?product=${product.Id}">
-      <img
-        src="${product.Images.PrimaryMedium}"
-        alt="Image of ${product.Name}"
-      />
-      <h3 class="card__brand">${product.Brand.Name}</h3>
-      <h2 class="card__name">${product.NameWithoutBrand}</h2>
-      <p class="product-card__price">$${product.FinalPrice}
+    return `${baseTemplate}
       <span class='list-price'><i><s>$${product.ListPrice}</i></s></span>
       <div class='discount-small'><b>${discount.toFixed(0)}% off</b>
       </div></p>
       </a>
     </li>`;
   } else {
-    return `<li class="product-card">
-    <a href="../product_pages/index.html?product=${product.Id}">
-    <img
-      src="${product.Images.PrimaryMedium}"
-      alt="Image of ${product.Name}"
-    />
-    <h3 class="card__brand">${product.Brand.Name}</h3>
-    <h2 class="card__name">${product.NameWithoutBrand}</h2>
-    <p class="product-card__price">$${product.FinalPrice}</p></a>
+    return `${baseTemplate}</p></a>
   </li>`;
   }
 }
@@ -65,4 +68,76 @@ function sortProducts(products, key, order) {
       return aValue < bValue ? 1 : -1;
     }
   });
+}
+
+async function toggleQuicklook(productId, display) {
+  const elQuicklookModal = document.querySelector('.product-modal');
+  const elOverlay = document.querySelector('.overlay');
+
+  if (display) {
+    const product = await findProductById(productId);
+    await renderWithTemplate(
+      productDetailTemplate,
+      elQuicklookModal,
+      '',
+      product
+    );
+
+    // Unhide the modal
+    elQuicklookModal.setAttribute('class', 'product-modal');
+    elOverlay.setAttribute('class', 'overlay');
+
+    document
+      .querySelector('.close-modal')
+      .addEventListener('click', (event) => {
+        toggleQuicklook(productId, false);
+      });
+  } else {
+    // Hide the modal
+    elQuicklookModal.setAttribute('class', 'product-modal hidden');
+    elOverlay.setAttribute('class', 'overlay hidden');
+  }
+}
+
+function productDetailTemplate(product) {
+  const baseTemplate = `
+  <span class="close-modal">X</span>
+  <section class="product-detail">
+  <h3 id="productName">${product.Name}</h3>
+
+  <h2 class="divider" id="productNameWithoutBrand">${product.NameWithoutBrand}</h2>
+
+  <img id="productImage" class="divider" src="${product.Images.PrimaryMedium}" alt="${product.Name}" />
+
+  <p class="product-card__price" id="productFinalPrice">`;
+
+  const endTemplate = `
+  <p class="product__color" id="productColorName">Color: ${product.Colors[0].ColorName}</p>
+
+  <p class="product__description" id="productDescriptionHtmlSimple">${product.DescriptionHtmlSimple}</p>
+
+  <div class="product-detail__add">
+    <span class="product__quantity" id="productQuantity">
+      qty: <span class="decrease-quantity" id="decreaseQuantity">-</span>
+      <span id="quantityNumber">1</span>
+      <span class="increase-quantity" id="increaseQuantity">+</span>
+    </span>
+    <button id="addToCart" data-id="${product.Id}">Add to Cart</button>
+  </div>
+</section>`;
+
+  if (product.FinalPrice < product.ListPrice) {
+    const discount =
+      ((product.ListPrice - product.FinalPrice) / product.ListPrice) * 100;
+
+    return `${baseTemplate} <div class='discount'><b>${discount.toFixed(
+      0
+    )}% off</b> deal</div>Price: $${product.FinalPrice}
+    <div class='list-price'><i>List Price: <s>$${
+      product.ListPrice
+    }</i></s></div>
+    </p> ${endTemplate}`;
+  } else {
+    return `${baseTemplate}Price: $${product.FinalPrice}</p> ${endTemplate}`;
+  }
 }
