0555325cb1e7717ef70e2eda51dbe4336d379a39
Kiktamo <mccrary@gmx.us>
2023-10-26 21:39:52 -0400

    Additional animation tweaks to add to cart

--- src/js/updateCartCount.mjs
+++ src/js/updateCartCount.mjs
@@ -1,6 +1,6 @@
 import { getLocalStorage } from './utils.mjs';
 
-export default function updateCartCount(bounce = false) {
+export default function updateCartCount(animate = false) {
   const cartElement = document.querySelector('.cart');
   const cartItems = getLocalStorage('so-cart');
   let count = 0;
@@ -15,17 +15,24 @@ export default function updateCartCount(bounce = false) {
   let cartCountEl =
     cartElement.querySelector('.cart-count') || document.createElement('span');
   cartCountEl.classList.add('cart-count');
+  const cartIcon = cartElement.querySelector('svg')
 
-  if (bounce) {
+  if (animate) {
     // Add a class to trigger the animation
     cartCountEl.style.animation = 'cartBounce 0.5s ease';
-    cartCountEl.classList.add('bounce');
 
     // Remove the class after the animation completes
     cartCountEl.addEventListener('animationend', () => {
-      cartCountEl.classList.remove('bounce');
+      cartCountEl.style.animation = '';
+    });
+
+    cartIcon.style.animation = 'cartShake 0.8s ease-in-out';
+
+    cartIcon.addEventListener('animationend', () => {
+      cartIcon.style.animation = '';
     });
   }
+
   cartCountEl.textContent = count.toString();
 
   // Add or remove the cart count element based on the count value
