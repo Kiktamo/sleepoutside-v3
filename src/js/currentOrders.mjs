81d19277045cc83a5f62450b6861b3d9ab15f846
Kiktamo <mccrary@gmx.us>
2023-11-07 19:09:29 -0500

    Final Updates

--- src/js/currentOrders.mjs
+++ src/js/currentOrders.mjs
@@ -1,12 +1,6 @@
-import {
-  qs
-} from './utils.mjs';
-import {
-  getOrders
-} from './externalServices.mjs';
-import {
-  renderListWithTemplate
-} from './utils.mjs';
+import { qs } from './utils.mjs';
+import { getOrders } from './externalServices.mjs';
+import { renderListWithTemplate } from './utils.mjs';
 
 export default async function currentOrders(selector, token, page = 1) {
   const allOrders = await getOrders(token);
@@ -39,21 +33,25 @@ async function createPageList(currentPage, numPages) {
   // Previous page
   if (currentPage > 1) {
     if (currentPage - 1 > 1) {
-      pages += `<li><a href="?page=1">1</a></li><li>...</li>`
+      pages += `<li><a href="?page=1">1</a></li><li>...</li>`;
     }
     pages += `
     <li><a href="?page=${currentPage - 1}">${currentPage - 1}</a></li>
     <li>${currentPage}</li>`;
   } else {
-    pages += `<li>${currentPage}</li>`
+    pages += `<li>${currentPage}</li>`;
   }
 
   // Next pages
-  for (let i = currentPage + 1; i <= Math.min(numPages - 1, currentPage + 4); i++) {
+  for (
+    let i = currentPage + 1;
+    i <= Math.min(numPages - 1, currentPage + 4);
+    i++
+  ) {
     pages += `<li><a href="?page=${i}">${i}</a></li>`;
   }
 
-  if (currentPage + 4 < numPages) {
+  if (currentPage + 4 < numPages - 1) {
     pages += `<li>...</li>`;
   }
 
@@ -68,10 +66,14 @@ async function createPageList(currentPage, numPages) {
 function orderCardTemplate(order) {
   var orderCard = `<li class="order-card">
   <h2 class="order__name"><b>Order For:</b> ${order.fname} ${order.lname}</h2>
-  <p class="order__date"><b>Order Date:</b> ${new Date(order.orderDate).toDateString()}</p>
-  <p class="order__total"><b>Order Total:</b> $${Number(order.orderTotal).toFixed(2)}</p><ul class="order-items">`;
+  <p class="order__date"><b>Order Date:</b> ${new Date(
+    order.orderDate
+  ).toDateString()}</p>
+  <p class="order__total"><b>Order Total:</b> $${Number(
+    order.orderTotal
+  ).toFixed(2)}</p><ul class="order-items">`;
 
-  order.items.forEach(item => {
+  order.items.forEach((item) => {
     orderCard += `<li>
     <p class="order-item-name"><b>Product:</b> ${item.name}</p>
     <p class="order-item-quantity"><b>Qty:</b> ${item.quantity}</p>
