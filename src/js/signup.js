b92b8e564c808e6ab7d550c21a52dd6afc3b5215
Joel Cannon <joelcannon@mac.com>
2023-11-09 20:19:20 -0600

    add form, css and validation logic.

--- src/js/signup.js
+++ src/js/signup.js
@@ -1,4 +1,5 @@
-import { loadHeaderFooter } from './utils.mjs';
+import { loadHeaderFooter, alertMessage, formDataToJSON } from './utils.mjs';
+import { signup } from './externalServices.mjs';
 
 loadHeaderFooter();
 
@@ -24,25 +25,49 @@ document.addEventListener('DOMContentLoaded', () => {
 
     if (password !== passwordConfirmation) {
       // Passwords do not match - display an error message in a popup
-      alert('Passwords do not match. Please try again.');
+      alertMessage('Passwords do not match. Please try again.');
     } else {
       // Submit the form if everything is valid
       // You can send the data to your server for user creation
       // Example: You can use fetch to post the data to your server
       // Here's a simplified example:
+      // try {
+      //   const response = await fetch('/users', {
+      //     method: 'POST',
+      //     headers: {
+      //       'Content-Type': 'application/json',
+      //     },
+      //     body: JSON.stringify({
+      //       email,
+      //       password,
+      //       fullName,
+      //       address,
+      //     }),
+      //   });
+      const json = formDataToJSON(form);
+      console.log(json);
       try {
-        const response = await fetch('/users', {
-          method: 'POST',
-          headers: {
-            'Content-Type': 'application/json',
-          },
-          body: JSON.stringify({
-            email,
-            password,
-            fullName,
-            address,
-          }),
-        });
+        const res = await signup(json);
+        console.log(res);
+
+        window.location.href = `./success.html?order=${encodeURIComponent(
+          res.orderId
+        )}`;
+        this.finish();
+      } catch (err) {
+        console.log(err);
+        // err.message.forEach((element) => {
+        //   alertMessage(element.toString());
+        // });
+        if (typeof err.message === 'object') {
+          for (const key in err.message) {
+            if (err.message.hasOwnProperty(key)) {
+              const element = err.message[key];
+              // Here, 'key' is the property name, and 'element' is the value.
+              alertMessage(element);
+            }
+          }
+        }
 
         if (response.status === 200) {
           // User creation successful
@@ -51,9 +76,6 @@ document.addEventListener('DOMContentLoaded', () => {
           // Handle server error responses or other cases
           alert('User creation failed. Please try again.');
         }
-      } catch (error) {
-        // Handle fetch or server communication errors
-        alert('An error occurred. Please try again.');
       }
     }
   });
